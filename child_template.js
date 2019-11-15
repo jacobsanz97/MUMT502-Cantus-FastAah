132518d3f18e2cb19750bd01f0b33b94dfa1ad97
Jacob <jacobsanz@gmail.com>
2019-11-15 14:50:28 -0500

    template

---
+++ child_template.js
@@ -0,0 +1,77 @@
+function volpiano2midi(input_str, note_dur) {
+	//construct dictionary with pitch values
+	var pitch_dict = {};
+	pitch_dict['9'] = 43;
+	pitch_dict['a'] = 45;
+	pitch_dict['b'] = 47;
+	pitch_dict['c'] = 48;
+	pitch_dict['d'] = 50;
+	pitch_dict['e'] = 52;
+	pitch_dict['f'] = 53;
+	pitch_dict['g'] = 55;
+	pitch_dict['h'] = 57;
+	pitch_dict['j'] = 59;
+	pitch_dict['k'] = 60;
+	pitch_dict['l'] = 62;
+	pitch_dict['m'] = 64;
+	pitch_dict['n'] = 65;
+	pitch_dict['o'] = 67;
+	pitch_dict['p'] = 69;
+	pitch_dict['q'] = 71;
+	pitch_dict['r'] = 72;
+	pitch_dict['s'] = 74;
+
+	//iterate through each character, check if in dictionary, play the corresponding pitch
+	for (var i = 0; i < input_str.length; i++) {
+		if (input_str.charAt(i) in pitch_dict){
+		  	player.queueWaveTable(audioContext, audioContext.destination, selectedPreset, 0, pitch_dict[input_str.charAt(i)], note_dur);
+		  	sleep(note_dur*1000);
+		}
+	}
+}
+
+//To delay the sequencer loop
+function sleep(milliseconds) {
+  	var start = new Date().getTime();
+  	for (var i = 0; i < 1e7; i++) {
+    	if ((new Date().getTime() - start) > milliseconds){
+    			break;
+    	}
+  	}
+}
+
+function parse_volpiano(syllableArray){
+	if (syllableArray.length < 1){
+		return;
+	}
+	var final = ""
+	for (var i = 0; i < syllableArray.length; i++) {
+		var current = syllableArray[i].textContent;
+		var vol_note = ""
+		for (var char = 0; char < current.length-1; i++){ //need the -1?
+			var curr_char = current.charAt(char);
+			var next_char = current.charAt(char+1);
+			var vol_note = vol_note.concat(curr_char);
+			if (curr_char == "-" && next_char != "-"){
+				final = final.concat(vol_note)
+				break;
+			}
+		}
+		
+	}
+	return final
+}
+
+function main(){
+	//gets the single panel that is expanded
+	//var expanded_panel = document.getElementsByClassName("panel-collapse collapse in");
+	//var current_volpianos = expanded_panel[0].getElementsByClassName("volpiano-syllable");
+
+	//var volString = parse_volpiano(current_volpianos)
+	var volString = 'abcdefglol'
+
+	var volTempo = 60.0/document.getElementById('myRange').value
+	volpiano2midi(volString, volTempo)
+}
+
+
